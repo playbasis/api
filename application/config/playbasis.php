47fd2ba4bce2a7a500c3093ec272d3652ee04d57
wee <wee@playbasis.com>
2013-12-16 18:48:50 +0700

    try to made https

--- application/config/playbasis.php
+++ application/config/playbasis.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['IMG_PATH'] = 'http://images.pbapp.net/';
+//$config['IMG_PATH'] = 'http://images.pbapp.net/';
+$config['IMG_PATH'] = 'https://d3mljzresf650e.cloudfront.net/';
 
 ?>
