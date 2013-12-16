f2314a1b1d44da45c1b69902d8afab325014bcdb
wee <wee@playbasis.com>
2013-12-16 13:54:28 +0700

    take out memcached

--- application/helpers/memcache_helper.php
+++ application/helpers/memcache_helper.php
@@ -11,16 +11,16 @@ function _get_memcached_result($mdl, $table, &$sql, &$memId)
 {
 	$sql = $mdl->site_db()->get_compiled_select($table);
 	$memId = _build_memcache_id($sql, $table, $mdl->get_site());
-	return $mdl->memcached_library->get($memId);
-//    return null;
+//	return $mdl->memcached_library->get($memId);
+    return null;
 }
 
 function _count_all_results_memcached_result($mdl, $table, &$sql, &$memId)
 {
 	$sql = $mdl->site_db()->get_compiled_count_all_results($table);
 	$memId = _build_memcache_id($sql, $table, $mdl->get_site());
-	return $mdl->memcached_library->get($memId);
-//    return null;
+//	return $mdl->memcached_library->get($memId);
+    return null;
 }
 
 function db_get_row_array($mdl, $table)
@@ -30,7 +30,7 @@ function db_get_row_array($mdl, $table)
 	{
 		$result = $mdl->site_db()->run_compiled_sql($sql);
 		$result = $result->row_array();
-		$mdl->memcached_library->set($memId, $result);
+//		$mdl->memcached_library->set($memId, $result);
 	}
 	return $result;
 }
@@ -42,7 +42,7 @@ function db_get_result_array($mdl, $table)
 	{
 		$result = $mdl->site_db()->run_compiled_sql($sql);
 		$result = $result->result_array();
-		$mdl->memcached_library->set($memId, $result);
+//		$mdl->memcached_library->set($memId, $result);
 	}
 	return $result;
 }
@@ -53,7 +53,7 @@ function db_count_all_results($mdl, $table)
 	if(!$result)
 	{
 		$result = $mdl->site_db()->run_compiled_sql_count_all_results($sql);
-		$mdl->memcached_library->set($memId, $result);
+//		$mdl->memcached_library->set($memId, $result);
 	}
 	return $result;
 }
