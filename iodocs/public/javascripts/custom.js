7589278f2e4abfe2b15b0eec66a3fa3ce297b3d3
vkado <wee@playbasis.com>
2014-02-07 14:13:57 +0700

    update iodoc

--- iodocs/public/javascripts/custom.js
+++ iodocs/public/javascripts/custom.js
@@ -3,45 +3,25 @@ $(document).ready(function(){
 	console.log('load custom.js')
 	 
 
-	/*$('.topbar_area img').on('click',function(){
+	$('.topbar_area img').on('click',function(){
 		// console.log('go to playbasis_website')
 		window.location = 'http://www.playbasis.com';
-	})*/
+	})
 
 		
-    $('.detail_enclosure').show('fast');
-    $('.apidoc_enclosure').hide('fast');
-    $('#detail_nav').addClass('active');
+		$('.detail_enclosure').show('fast');
+		$('.apidoc_enclosure').hide('fast');
 
 	$('#detail').on('click',function(){
-        $('.nav_menu').removeClass('active');
-        $('#api_nav').addClass('active');
 		$('.detail_enclosure').hide('fast');  
 		$('.apidoc_enclosure').show('fast');
 	})
 
 
 	$('#api').on('click',function(){
-        $('.nav_menu').removeClass('active');
-        $('#detail_nav').addClass('active');
 		$('.detail_enclosure').show('fast');
 		$('.apidoc_enclosure').hide('fast');
 	})
-
-    $('#detail_nav').on('click',function(){
-        $('.nav_menu').removeClass('active');
-        $('#detail_nav').addClass('active');
-        $('.detail_enclosure').show('fast');
-        $('.apidoc_enclosure').hide('fast');
-    })
-
-
-    $('#api_nav').on('click',function(){
-        $('.nav_menu').removeClass('active');
-        $('#api_nav').addClass('active');
-        $('.detail_enclosure').hide('fast');
-        $('.apidoc_enclosure').show('fast');
-    })
 
 
 	// $('#detail').trigger('click')
