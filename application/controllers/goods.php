c8b796b2ed4746f90102ac36e4267fd87c40774a
Piya Phuakwilai <piya.p@playbasis.com>
2017-07-04 11:45:45 +0700

    revert 2.31.7 , 2.31.9

--- application/controllers/goods.php
+++ application/controllers/goods.php
@@ -108,8 +108,7 @@ class Goods extends REST2_Controller
                 $data['selected_field'] = array_values($data['selected_field']);
             }
             
-            $active_filter = $this->input->get('active_filter') == "true" ? true : false;
-            if ($active_filter) {
+            if ($this->input->get('active_filter') == "true") {
                 if (!$this->input->get('date_start') && !$this->input->get('date_end')) {
                     $data['date_start'] = new MongoDate();
                 }
@@ -187,7 +186,7 @@ class Goods extends REST2_Controller
                     if($c_goods['is_group']){
                         array_push($in_group, $c_goods['name']);
                     } else {
-                        $custom_goods_id =$this->goods_model->getGoodsIDByName($this->client_id, $this->site_id, $c_goods['name'], null, $active_filter);
+                        $custom_goods_id =$this->goods_model->getGoodsIDByName($this->client_id, $this->site_id, $c_goods['name']);
                         array_push($goods_param_id, new MongoId($custom_goods_id));
                     }
                 }
@@ -198,7 +197,7 @@ class Goods extends REST2_Controller
             
             $in_goods = array();
             foreach ($group_list as $group_name) {
-                $goods_group_detail = $this->goods_model->getGoodsIDByName($this->client_id, $this->site_id, "", $group_name['name'], $active_filter);
+                $goods_group_detail = $this->goods_model->getGoodsIDByName($this->client_id, $this->site_id, "", $group_name['name']);
                 array_push($in_goods, new MongoId($goods_group_detail));
             }
             if ($filter_goods_name) {
