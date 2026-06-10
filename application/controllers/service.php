0e2777499d0881daa7f44922a942e4ca057c32d8
RZForemost <robertz@foremost-ai.com>
2026-06-10 17:19:51 +0700

    Disable service reset point endpoint

--- application/controllers/service.php
+++ application/controllers/service.php
@@ -332,6 +332,9 @@ class Service extends REST2_Controller
 
     public function reset_point_post()
     {
+        show_404('Service/reset_point');
+        return;
+
         $reward_name = $this->input->post('point_name');
 
         if (strtolower($reward_name) == "exp") {
